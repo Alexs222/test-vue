05ce2ef707d80d50cfda81b691284158ed5c6fb9
alexs222 <aleks.nikulin@gmail.com>
2018-05-02 10:08:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  #app
+    test-comp(
+      title="Hello from test!"
+      @buttonWasClicked="handleEmit"
+    )
+      .one(slot='top')
+        h1 top
+      .two(slot='bottom')
+        h3 bottom
+      .other(slot='default')
+        h5 default
+</template>
+
+
+<script>
+import testComponent from './components/test'
+export default {
+  components: {
+    testComp: testComponent
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleEmit(name, counter, isActive) {
+      console.log(name, counter, isActive);
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+    div
+        h1 {{title}}
+        button(
+            type='button'
+            @click='emitClick'
+        ) Кнопка
+        .wrap
+            .top
+                slot(name='top')
+            .bottom
+                slot(name='bottom')
+            .default
+                slot(name='default')
+
+</template>
+
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: "Hello world"
+        }
+    },
+    methods: {
+        emitClick() {
+            this.$emit('buttonWasClicked', 'sting', 12345, true);
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
+
