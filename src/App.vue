43e2a3ede931225f984dc62463c00695f17ef830
alexs222 <aleks.nikulin@gmail.com>
2018-05-02 20:40:40 +0200

    add v-model

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         h1 top
       .two(slot='bottom')
         h3 bottom
-      .other(slot='default')
+      .other
         h5 default
 </template>
 
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,23 +1,38 @@
 <template lang="pug">
     div
-        h1 {{title}}
-        button(
-            type='button'
-            @click='emitClick'
-        ) Кнопка
-        .wrap
-            .top
-                slot(name='top')
-            .bottom
-                slot(name='bottom')
-            .default
-                slot(name='default')
+        input(type="text" v-model="name" @keydown.enter="handleChange")
+        br
+        input(type="checkbox" value="one" v-model="counter")
+        .span one
+        input(type="checkbox" value="two" v-model="counter")
+        .span two
+        input(type="checkbox" value="three" v-model="counter")
+        .span three
+      
+        //- h1 {{title}}
+        //- button(
+        //-     type='button'
+        //-     @click='emitClick'
+        //- ) Кнопка
+        //- .wrap
+        //-     .top
+        //-         slot(name='top')
+        //-     .bottom
+        //-         slot(name='bottom')
+        //-     .default
+        //-         slot(name='default')
 
 </template>
 
 
 <script>
 export default {
+    data() {
+        return {
+            name: "",
+            counter: ["one", "three"]
+        }
+    },
     props: {
         title: {
             type: String,
@@ -27,6 +42,9 @@ export default {
     methods: {
         emitClick() {
             this.$emit('buttonWasClicked', 'sting', 12345, true);
+        },
+        handleChange() {
+            this.name = "";
         }
     }
 
